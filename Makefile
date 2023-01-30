30150950b1b217c1b82307f596b2d4b99758fbfa
Ulysseclem <51092179+Ulysseclem@users.noreply.github.com>
2023-01-30 14:51:19 +0100

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,52 @@
+DIRSRC		= ./srcs/
+
+DIRINC		= ./includes/
+
+DIROBJ		= ./objs/
+
+DIRLIB		= ./libft/
+
+SRC			= ft_printf
+
+SRCS		= ./srcs/ft_printf.c
+
+HEAD		= ./includes/ 
+
+OBJS		= ${SRCS:.c=.o}
+
+NAME		= libftprintf.a
+
+NAMELFT		= libft.a
+
+CC			= gcc
+RM			= rm -f
+AR			= ar rc
+RN			= ranlib
+
+CFLAGS		= -Wall -Wextra -Werror
+
+.c.o:
+			${CC} ${CFLAGS} -c -I${DIRINC} -I${DIRLIB} $< -o ${<:.c=.o}
+
+$(NAME):	${OBJS}
+			cd ${DIRLIB} && ${MAKE} && cp -v ${NAMELFT} ../${NAME}
+			${AR} ${NAME} ${OBJS}
+			${RN} ${NAME}
+
+test:		$(NAME) $(SRCS)
+			@ ${CC} -I ${DIRINC} -I ${DIRLIB} ./test/main.c ${NAME} && ./a.out
+
+all:		$(NAME)
+
+clean:
+			${RM} ${OBJS}
+			cd ${DIRLIB} && ${MAKE} clean
+
+fclean:		clean
+			${RM} $(NAME)
+			${RM} a.out
+			cd ${DIRLIB} && ${MAKE} fclean
+
+re:			fclean all
+
+.PHONY:		all clean fclean re
---
+++ includes/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uclement <uclement@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 10:57:22 by uclement          #+#    #+#             */
+/*   Updated: 2023/01/30 12:15:06 by uclement         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "libft.h"
+
+int		ft_printf(int);
+
+#endif
