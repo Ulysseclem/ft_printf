09bfbf46fafb54b15d980886db4e3ffbd4ed1ae7
Ulysse CLEMENT <uclement@e2r11p22.clusters.42paris.fr>
2023-03-03 11:41:49 +0100

    omly22

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: uclement <uclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 11:03:27 by uclement          #+#    #+#             */
-/*   Updated: 2023/03/03 11:28:53 by uclement         ###   ########.fr       */
+/*   Updated: 2023/03/03 11:39:44 by uclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_which(const char *str, va_list args)
 	else if (*str == 's')
 		ft_str(va_arg (args, char *));
 	else if (*str == 'p')
-		ft_pointer(va_arg (args, int)); // faux
+		return (3);
 	else if (*str == 'd')
 		ft_decimal(va_arg (args, double));
 	else if (*str == 'i')
--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: uclement <uclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 11:46:37 by uclement          #+#    #+#             */
-/*   Updated: 2023/03/03 11:27:49 by uclement         ###   ########.fr       */
+/*   Updated: 2023/03/03 11:40:27 by uclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 int	main(void)
 {
 
-	int a = 10;
-	int *b = &a;
 
 	ft_printf("test 'c' je dois affficher : %c = U \n",'U');
 	ft_printf("test 's' je dois affficher : %s = test \n","test");
@@ -27,8 +25,7 @@ int	main(void)
 	ft_printf("test 'd' je dois affficher : %d\n",10);
 	ft_printf("test 'x' je dois affficher : %x\n",1234);
 	ft_printf("test 'X' je dois affficher : %X\n",1234);
-	printf("test 'p' je dois affficher : %p\n",b);
-	ft_printf("test 'p' je dois affficher : %p\n",b);
+	printf("test 'p' je dois affficher : %p\n",(void *)30);
 
 
 }
